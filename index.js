71b0d4f828071d3db8fd0ff7b2532cbb3d724365
Ali Raza <aliraza.12748g@gmail.com>
2023-12-03 21:51:10 +0500

    imporve index file issues

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const gigRoutes = require("./routes/api/gigRoutes");
 const paymentRoutes = require("./routes/api/paymentRoutes");
 const orderRoutes = require("./routes/api/orderRoutes");
 const cors =require('cors')
-const isProduction = NODE_ENV === "production";
+// const isProduction = NODE_ENV === "production";
 
-if (!isProduction) {
+// if (!isProduction) {
 require("dotenv").config();
-}
+// }
 app.use(cors(
   {
     origin: "*",
@@ -27,13 +27,13 @@ require("./startup/session")(app); // Initialized session for authentication
 require("./startup/securityHeaders")(app); // Setting security headers with helmet module
 require("./startup/routes")(app); // Initializing all api routes
 
-if (isProduction) {
-  app.use(express.static("../client/build"));
+// if (isProduction) {
+//   app.use(express.static("../client/build"));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-} else {
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//   });
+// } else {
 
 app.use(express.static("../public"));
 app.get("/hello", (req, res) => {
@@ -42,7 +42,7 @@ app.get("/hello", (req, res) => {
 app.use("/api/gig", gigRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/payments", paymentRoutes);
-}
+// }
 
 app.listen(PORT, () => {
   console.log(`> 🚀 Server is running on port ${PORT}...`);
